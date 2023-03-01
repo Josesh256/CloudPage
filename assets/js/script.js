24a9018e9f865b5e42a0b877d7c8aafedd5c376c
unknown <currentq99@gmail.com>
2023-03-01 21:20:48 +0100

    Added new 3D isometric images, add 2 swiper images , add scrollup

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,8 +2,6 @@ const navMenu = document.getElementById("nav-menu"),
   navToggle = document.getElementById("nav-toggle"),
   navClose = document.getElementById("nav-close");
 
-
-
 if (navToggle) {
   navToggle.addEventListener("click", () => {
     navMenu.classList.add("show-menu");
@@ -16,14 +14,43 @@ if (navClose) {
   });
 }
 
-const navLink = document.querySelectorAll(".nav__link")
-function linkAction(){
-    const navMenu = document.getElementById('nav-menu')
-    navMenu.classList.remove('show-menu')
+const navLink = document.querySelectorAll(".nav__link");
+function linkAction() {
+  const navMenu = document.getElementById("nav-menu");
+  navMenu.classList.remove("show-menu");
 }
 
-navLink.forEach(item=>item.addEventListener("click",linkAction))
+navLink.forEach((item) => item.addEventListener("click", linkAction));
 
+//SWIPER
+
+let homeSwiper = new Swiper(".home-swiper", {
+  spaceBetween: 30,
+  loop: "true",
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
+
+//SWIPER 2
+
+let newSwiper = new Swiper(".new-swiper", {
+  centeredSlides: true,
+  slidesPerView: "auto",
+  loop: "true",
+  spaceBetween: 20,
+});
+
+//Scroll up
+
+function scrollUp(){
+  const scrollup = document.getElementById("scroll-up")
+  if(this.scrollY >= 460) scrollup.classList.add("show-scroll");
+  else scrollup.classList.remove("show-scroll");
+}
+window.addEventListener("scroll", scrollUp)
 //Scrollreveal
 
 const sr = ScrollReveal({
@@ -36,9 +63,12 @@ const sr = ScrollReveal({
 sr.reveal(`.home-swiper, 
 .new-swiper,.newsletter__container`);
 
-sr.reveal(`.category__data,
- .trick__content, .footer__content`,{interval:100});
+sr.reveal(
+  `.category__data,
+ .trick__content, .footer__content`,
+  { interval: 100 }
+);
 
-sr.reveal(`.about__data, .discount__img`,{origin:"left"} )
+sr.reveal(`.about__data, .discount__img`, { origin: "left" });
 
-sr.reveal(`.about__img, .discount__data`,{origin:"right"} )
+sr.reveal(`.about__img, .discount__data`, { origin: "right" });
